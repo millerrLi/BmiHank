ac955f6d14fbdbabae47f4c84abef543e3c7ac2d
millerr <s0950311@gmailcom>
2024-04-21 13:31:57 +0800

    Constructor

--- app/src/main/java/com/millerr/bmi/Hello.kt
+++ app/src/main/java/com/millerr/bmi/Hello.kt
@@ -6,11 +6,24 @@ class Hello {
 
 //筆記：val設定值後不可以改變，不可以再次assign
 //筆記：var設定值後可以再次改變，可以再次assign
+//筆記：快捷鍵：Alt + Shift + 方向鍵上下，可以讓該行的程式碼向上或向下
+//筆計：快捷鍵：Ctrl + D：複製該行的程式碼, Ctrl + Y：刪除該行的程式碼
+//筆記：快捷鍵：Ctrl + P：顯示物件，方法要輸入的參數提醒
 fun main() {
-    val p = Person()
+    val p = Person("Li", 88.2f, 1.7f)
+    println("Person的BMI: ${p.bmi()}")
+    val millerr = Person("millerr", 78f, 1.82f)
+    println("${p.name}的BMI：${p.bmi()}")
+    println("${millerr.name}的BMI：${millerr.bmi()}")
     p.hello()
 
-    println("Hello Kotlin！")
+    //BMI
+    val w = 86.6f
+    val h = 1.71f
+    val bmi = w / (h * h)
+    println("BMI: ${bmi}")
+
+/*
     //Byte, Short, Int, Long
     //不管是多大多小的數值，都建議用Int，不用因為數值小，就用short, Byte來表示
     //這樣會造成後續人的不便
@@ -29,5 +42,7 @@ fun main() {
     val isAdult = false
     //Char
     val c = 'A'
-    println(c)
+    println(c)*/
+
+
 }
--- app/src/main/java/com/millerr/bmi/Person.kt
+++ app/src/main/java/com/millerr/bmi/Person.kt
@@ -1,6 +1,11 @@
 package com.millerr.bmi
 
-class Person {
+class Person constructor(val name: String, val weight: Float, val height: Float) {
+    fun bmi(): Float {
+        var bmi = weight / (height * height)
+        return bmi
+    }
+
     fun hello() {
         println("Hello")
     }
